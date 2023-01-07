d677be6043ab9e1b9094be877423402e76e346ef
kstoyanov135 <121305077+kstoyanov135@users.noreply.github.com>
2023-01-07 18:24:50 +0200

    Update index.js

--- Quiz Game/index.js
+++ Quiz Game/index.js
@@ -2,6 +2,16 @@ const questionContainers = document.querySelectorAll('.question-container');
 let numCorrect = 0;
 let currentQuestion = 0;
 
+// Define a reset function
+const reset = () => {
+  numCorrect = 0;
+  currentQuestion = 0;
+  questionContainers.forEach(container => {
+    container.style.display = 'none';
+  });
+  questionContainers[0].style.display = 'block';
+}
+
 questionContainers.forEach((container, index) => {
   // Get the answer choices
   const answers = container.querySelectorAll('.answer');
@@ -19,8 +29,9 @@ questionContainers.forEach((container, index) => {
         questionContainers[currentQuestion].style.display = 'block';
         container.style.display = 'none';
       } else {
-        // When all the questions have been answered, display the final score
+        // When all the questions have been answered, display the final score and reset the game after 0,2 seconds
         alert(`Your score is ${numCorrect}/${questionContainers.length}`);
+        setTimeout(reset, 0,2000);
       }
     });
   });
